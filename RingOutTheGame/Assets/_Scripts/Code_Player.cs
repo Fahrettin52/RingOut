a062ca5950c6b12cdc2014e75e51520ecc99615d
Fahrettin <u.f.gungor@hotmail.com>
2018-03-05 15:21:21 +0100

    Character movement

---
+++ RingOutTheGame/Assets/_Scripts/Code_Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Code_Player : MonoBehaviour {
+
+    public int movementSpeed;
+    public int rotationSpeed;
+
+    public Vector3 input;
+
+    public Rigidbody rigidbod;
+
+    public Vector3 targetRotation;
+
+    private void Start()
+    {
+        rigidbod = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Movement();
+	}
+
+    /// <summary>
+    /// Handles the movement and otation of the player
+    /// </summary>
+    private void Movement()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (move != Vector3.zero) {
+            transform.rotation = Quaternion.LookRotation(move * Time.deltaTime);
+        }
+
+        transform.Translate(move * movementSpeed * Time.deltaTime, Space.World);
+    }
+}
