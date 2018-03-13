d4cd08c5042b4abbbd1a4575cf2f6eb0f2a053ae
kvk4life <mackmendesmoreira@gmail.com>
2018-03-13 14:47:08 +0100

    Stamina regeneration en consumption
    Heb stamina regeneration en consumption toegevoegd in het project. Je kunt nu aanvallen met spatie zal strax ook doen dat je kunt aanvallen met XButton. Aanvallen kost nu stamina.
    
    Heb ook de stamina regen aangemaakt zodat de speler regenerate en heb mijn code soft gemaakt. Staat momenteel geen hardcoded stuff meer in.

--- RingOutTheGame/Assets/_Scripts/Code_Player.cs
+++ RingOutTheGame/Assets/_Scripts/Code_Player.cs
@@ -5,32 +5,54 @@ public class Code_Player : MonoBehaviour {
 
     public int movementSpeed;
     public int rotationSpeed;
+    [Header("Stamina Related")]
     public int stamina;
+    public int attackCost; // Determines how much stamina is consumed after attacking
+    private int startStamina;
+    public int staminaRegenAmount; // With how much the stamina regenerates per rate
+    public float staminaRegenRate; // The rate per tick that stamina regenerates
+
     [Header("Knockback")]
-    public int knockbackSpeed;    
-    
-    public float knockbackTime;
-    public float smoothingRate;
+    public int knockbackSpeed; // How fast (by proxy how far) the PC will move when knockedback
+    private int startKnockbackSpeed;
+
+    public float knockbackTime; // How long the knockback effect lasts
+    public float smoothingRate; // Determines how smoothly the knockbackSpeed decreases in value. Advised to be 10f
     private float knockbackDecreaser;
+    private float staminaRegen;
 
-    private Vector3 knockbackDir;
+    [Header("Based on players remaining stamina")]
+    public int[] knockbackDangerLevels; // The dangerlevels of having low stamina. A lower amount means a higher knockbackMultiplier
+    [Header("Based on the array above (Has to be same size)")]
+    public float[] knockbackMultiplierList; // Connected to knockbackDangerLevels. Determine when to use which multiplier
 
-    private bool mayMove;
+    private Vector3 knockbackDir; // Direction the knockback will move in
+
+    private bool mayMove; // Determines whether or not the player has control over the PCs movement. True = yes, False = no
 
     // Use this for initialization
     private void Start() {
+        SetStartVariables();        
         ToggleMayMove();
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update() {
         if (mayMove) {
             Movement();
         }
         else {
             Knockback();
         }
-	}
+
+        if (Input.GetButtonDown("Jump")) {
+            Attack();
+        }
+
+        if (stamina < startStamina) {
+            StaminaRegen();
+        }
+    }
 
     /// <summary>
     /// Handles the movement and rotation of the player
@@ -44,6 +66,16 @@ public class Code_Player : MonoBehaviour {
         transform.Translate(move * movementSpeed * Time.deltaTime, Space.World);
     }
 
+    // Called from the Attack/Action buttons
+    public void Attack() {
+        // Making sure that stamina never gets below 0
+        if (stamina - attackCost >= 0) {
+            // TODO activate the shield here
+
+            stamina -= attackCost;
+        }
+    }
+
     // Starts the knockback sequence
     public void StartKnockback(Vector3 hitPosition) {
         // Calculate the knockBackDir
@@ -55,11 +87,11 @@ public class Code_Player : MonoBehaviour {
         // Disallow movement
         ToggleMayMove();
         StartCoroutine(KnockbackCountdown());
-    }    
+    }
 
     // Is called when mayMove is false and translates the PC into it's appropraite direction. Until mayMove is true again
     private void Knockback() {
-        transform.Translate(knockbackDir * knockbackSpeed * knockbackMultiplier() * Time.deltaTime, Space.World);
+        transform.Translate(knockbackDir * knockbackSpeed * KnockbackMultiplier() * Time.deltaTime, Space.World);
         KnockbackSmoother();
     }
 
@@ -69,37 +101,43 @@ public class Code_Player : MonoBehaviour {
             knockbackSpeed--;
             knockbackDecreaser = Time.time + (knockbackTime / smoothingRate);
         }
-    }   
+    }
 
     // Countdown for knockback. When it's done it resets the appropriate variables and call ToggleMayMove()
     private IEnumerator KnockbackCountdown() {
         yield return new WaitForSeconds(knockbackTime);
-        knockbackSpeed = 10;
+        knockbackSpeed = startKnockbackSpeed;
         ToggleMayMove();
     }
 
     // Increases the knockbackSpeed depending on how much stamina the PC has left.
-    private float knockbackMultiplier() {
-        if (stamina > 75) {
-            return 1f;
+    private float KnockbackMultiplier() {
+        for (int i = 0; i < knockbackDangerLevels.Length; i++) {
+            // Checks if the current stamina is lower than the current danger level
+            if (stamina < knockbackDangerLevels[i]) {
+                return knockbackMultiplierList[i];
+            }
         }
-        else if (stamina > 50) {
-            return 1.5f;
-        }
-        else if (stamina > 25) {
-            return 2f;
-        }
-        else if (stamina > 10) {
-            return 3f;
-        }
-        else {
-            return 4f;
-        }
+        return 1f;
     }
 
     // Toggles the mayMove variable
     private void ToggleMayMove() {
         mayMove = !mayMove;
+    }
+
+    // Regenerates the PCs stamina when it's necesary
+    private void StaminaRegen() {
+        if (Time.time > staminaRegen) {
+            stamina += staminaRegenAmount;
+            staminaRegen = Time.time + staminaRegenRate;
+        }
+    }
+
+    // Sets any variable that needs to be set during Start()
+    private void SetStartVariables (){
+        startStamina = stamina;
+        startKnockbackSpeed = knockbackSpeed;
     }
 
     // Is the only Function that call StartKnockback() and should be removed/changed once the shields are being implemented
