28c4918e1e99d475acae502f2b24e1703c764edb
kvk4life <mackmendesmoreira@gmail.com>
2018-03-13 13:11:01 +0100

    knockback geadd
    Weet niet hoe efficient het daadwerkelijk is maar goed.
    Moet nog alleen de knockbackMulitplier ratio softcoden inplaats van hardcoden I guess. Maar ga nu eerst even pauze houden. Als je het wilt testen loop gewoon tegen de "muur" aan die op de arena staat.

--- RingOutTheGame/Assets/_Scripts/Code_Player.cs
+++ RingOutTheGame/Assets/_Scripts/Code_Player.cs
@@ -1,23 +1,35 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Code_Player : MonoBehaviour {
 
     public int movementSpeed;
     public int rotationSpeed;
+    public int stamina;
+    [Header("Knockback")]
+    public int knockbackSpeed;    
+    
+    public float knockbackTime;
+    public float smoothingRate;
+    private float knockbackDecreaser;
 
-    public Vector3 input;
-    public Vector3 targetRotation;
+    private Vector3 knockbackDir;
 
-    public Rigidbody rigidbod;
+    private bool mayMove;
 
     // Use this for initialization
     private void Start() {
-        rigidbod = GetComponent<Rigidbody>();
+        ToggleMayMove();
     }
 
     // Update is called once per frame
     void Update () {
-        Movement();
+        if (mayMove) {
+            Movement();
+        }
+        else {
+            Knockback();
+        }
 	}
 
     /// <summary>
@@ -30,5 +42,70 @@ public class Code_Player : MonoBehaviour {
         }
 
         transform.Translate(move * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    // Starts the knockback sequence
+    public void StartKnockback(Vector3 hitPosition) {
+        // Calculate the knockBackDir
+        knockbackDir = hitPosition - transform.position;
+        knockbackDir = -knockbackDir.normalized;
+        // Controls the players "hop" in the air
+        knockbackDir.y = 0f;
+
+        // Disallow movement
+        ToggleMayMove();
+        StartCoroutine(KnockbackCountdown());
+    }    
+
+    // Is called when mayMove is false and translates the PC into it's appropraite direction. Until mayMove is true again
+    private void Knockback() {
+        transform.Translate(knockbackDir * knockbackSpeed * knockbackMultiplier() * Time.deltaTime, Space.World);
+        KnockbackSmoother();
+    }
+
+    // Smooths out the knockback for a bit
+    private void KnockbackSmoother() {
+        if (Time.time > knockbackDecreaser) {
+            knockbackSpeed--;
+            knockbackDecreaser = Time.time + (knockbackTime / smoothingRate);
+        }
+    }   
+
+    // Countdown for knockback. When it's done it resets the appropriate variables and call ToggleMayMove()
+    private IEnumerator KnockbackCountdown() {
+        yield return new WaitForSeconds(knockbackTime);
+        knockbackSpeed = 10;
+        ToggleMayMove();
+    }
+
+    // Increases the knockbackSpeed depending on how much stamina the PC has left.
+    private float knockbackMultiplier() {
+        if (stamina > 75) {
+            return 1f;
+        }
+        else if (stamina > 50) {
+            return 1.5f;
+        }
+        else if (stamina > 25) {
+            return 2f;
+        }
+        else if (stamina > 10) {
+            return 3f;
+        }
+        else {
+            return 4f;
+        }
+    }
+
+    // Toggles the mayMove variable
+    private void ToggleMayMove() {
+        mayMove = !mayMove;
+    }
+
+    // Is the only Function that call StartKnockback() and should be removed/changed once the shields are being implemented
+    public void OnCollisionEnter(Collision col) {
+        if (col.transform.name == "Bouncer") {
+            StartKnockback(col.transform.position);
+        }
     }
 }
