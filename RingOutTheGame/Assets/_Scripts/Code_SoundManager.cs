480d1f0ec9f14e3a24406f07a71f875e28d79a4a
Fahrettin <u.f.gungor@hotmail.com>
2018-03-23 15:42:49 +0100

    Soundmanager update
    - Master volume work properly

--- RingOutTheGame/Assets/_Scripts/Code_SoundManager.cs
+++ RingOutTheGame/Assets/_Scripts/Code_SoundManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Code_SoundManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class Code_SoundManager : MonoBehaviour
     public AudioClip[] sFXAudioClip;
     public AudioClip[] ambientAudioClip;
     public AudioClip[] characterAudioClip;
+
+    public Toggle[] volumeChecks;
 
     // Use this for initialization
     void Start()
@@ -66,61 +69,110 @@ public class Code_SoundManager : MonoBehaviour
 
     public void TurnOffSelectedSound(int index)
     {
-        switch (index)
+        if (volumeChecks[0].isOn)
         {
-            case 0:
-                foreach (AudioSource item in musicAudioSource)
-                {
-                    item.mute = !item.mute;
-                }
-                break;
+            switch (index)
+            {
+                case 0:
+                    foreach (AudioSource item in musicAudioSource)
+                    {
+                        item.mute = !item.mute;
+                    }
+                    break;
 
-            case 1:
-                foreach (AudioSource item in sFXAudioSource)
-                {
-                    item.mute = !item.mute;
-                }
-                break;
+                case 1:
+                    foreach (AudioSource item in sFXAudioSource)
+                    {
+                        item.mute = !item.mute;
+                    }
+                    break;
 
-            case 2:
-                foreach (AudioSource item in ambientAudioSource)
-                {
-                    item.mute = !item.mute;
-                }
-                break;
+                case 2:
+                    foreach (AudioSource item in ambientAudioSource)
+                    {
+                        item.mute = !item.mute;
+                    }
+                    break;
 
-            case 3:
-                foreach (AudioSource item in characterAudioSource)
-                {
-                    item.mute = !item.mute;
-                }
-                break;
+                case 3:
+                    foreach (AudioSource item in characterAudioSource)
+                    {
+                        item.mute = !item.mute;
+                    }
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
         }
+
     }
 
     public void TurnOffAllSounds()
     {
-        for (int i = 0; i < musicAudioSource.Length; i++)
+        if (!volumeChecks[0].isOn)
         {
-            musicAudioSource[i].mute = !musicAudioSource[i].mute;
+            for (int i = 0; i < musicAudioSource.Length; i++)
+            {
+                musicAudioSource[i].mute = true;
+            }
+
+            for (int i = 0; i < sFXAudioSource.Length; i++)
+            {
+                sFXAudioSource[i].mute = true;
+            }
+
+            for (int i = 0; i < ambientAudioSource.Length; i++)
+            {
+                ambientAudioSource[i].mute = true;
+            }
+
+            for (int i = 0; i < characterAudioSource.Length; i++)
+            {
+                characterAudioSource[i].mute = true;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < musicAudioSource.Length; i++)
+            {
+                musicAudioSource[i].mute = !volumeChecks[1].isOn;
+            }
+
+            for (int i = 0; i < sFXAudioSource.Length; i++)
+            {
+                sFXAudioSource[i].mute = !volumeChecks[2].isOn;
+            }
+
+            for (int i = 0; i < ambientAudioSource.Length; i++)
+            {
+                ambientAudioSource[i].mute = !volumeChecks[3].isOn;
+            }
+
+            for (int i = 0; i < characterAudioSource.Length; i++)
+            {
+                characterAudioSource[i].mute = !volumeChecks[4].isOn;
+            }
         }
 
-        for (int i = 0; i < sFXAudioSource.Length; i++)
-        {
-            sFXAudioSource[i].mute = !sFXAudioSource[i].mute;
-        }
+        //for (int i = 0; i < musicAudioSource.Length; i++)
+        //{
+        //    musicAudioSource[i].mute = !musicAudioSource[i].mute;
+        //}
 
-        for (int i = 0; i < ambientAudioSource.Length; i++)
-        {
-            ambientAudioSource[i].mute = !ambientAudioSource[i].mute;
-        }
+        //for (int i = 0; i < sFXAudioSource.Length; i++)
+        //{
+        //    sFXAudioSource[i].mute = !sFXAudioSource[i].mute;
+        //}
 
-        for (int i = 0; i < characterAudioSource.Length; i++)
-        {
-            characterAudioSource[i].mute = !characterAudioSource[i].mute;
-        }
+        //for (int i = 0; i < ambientAudioSource.Length; i++)
+        //{
+        //    ambientAudioSource[i].mute = !ambientAudioSource[i].mute;
+        //}
+
+        //for (int i = 0; i < characterAudioSource.Length; i++)
+        //{
+        //    characterAudioSource[i].mute = !characterAudioSource[i].mute;
+        //}
     }
 }
